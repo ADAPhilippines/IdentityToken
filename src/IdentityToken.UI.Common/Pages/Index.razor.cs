90551c8c355a5d3208829e1031cd42f9c1825ce4
Perkins Jon Ong <official.pjo@outlook.com>
2021-09-11 13:02:31 +0800

    adjusted panel switching behavior particularly removing scrollTop behavior

--- src/IdentityToken.UI.Common/Pages/Index.razor.cs
+++ src/IdentityToken.UI.Common/Pages/Index.razor.cs
@@ -5,21 +5,26 @@ namespace IdentityToken.UI.Common.Pages
 {
     partial class Index
     {
-        [Parameter] public string Page { get; set; } = "mint";
+        private string ActivePanel { get; set; } = "mint";
 
         protected override void OnInitialized()
         {
-            Page ??= "mint";
+            ActivePanel ??= "mint";
             base.OnInitialized();
         }
 
         private string GetTabTextColorClass(string tabName)
         {
-            return tabName == Page ? "text-idt-indigo" : "text-idt-gray";
+            return tabName == ActivePanel ? "text-idt-indigo" : "text-idt-gray";
         }
         private string GetTabUnderlineColorClass(string tabName)
         {
-            return tabName == Page ? "bg-idt-indigo" : "bg-transparent";
+            return tabName == ActivePanel ? "bg-idt-indigo" : "bg-transparent";
+        }
+
+        private void SwitchActivePanel(string panel)
+        {
+            ActivePanel = panel;
         }
     }
 }
