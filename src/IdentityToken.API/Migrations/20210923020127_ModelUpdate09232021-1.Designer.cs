b95e95d5c29ccd7920e26e6e4b7bfb1c4cbab9b3
Clark Alesna <clark_alesna@hotmail.com>
2021-09-23 02:03:18 +0000

    Migration for CardanoIdentityToken Metadata

--- src/IdentityToken.API/Migrations/20210923020127_ModelUpdate09232021-1.Designer.cs
+++ src/IdentityToken.API/Migrations/20210923020127_ModelUpdate09232021-1.Designer.cs
@@ -4,15 +4,17 @@ using System.Text.Json;
 using IdentityToken.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace IdentityToken.API.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210923020127_ModelUpdate09232021-1")]
+    partial class ModelUpdate092320211
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
